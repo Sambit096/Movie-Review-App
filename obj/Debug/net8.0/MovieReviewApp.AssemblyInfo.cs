9535b28e8d24f6d6c9415fc72762fb75899367f0
Adams <keeferadams@gmail.com>
2024-10-22 17:03:05 -0400

    cleaning up

--- obj/Debug/net8.0/MovieReviewApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieReviewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639f2f315d270820e5b69309afc3dd621e5b0865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b50b7e3cd528aba78f564f05eecd41543864daa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
