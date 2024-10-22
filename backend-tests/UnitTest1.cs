7e85827bfb9ae4a175c5aaa2dc1ecd3d34160450
andyjDavison <andyd.soccer@gmail.com>
2024-10-22 17:32:07 -0400

    Create NUnit test project for testing backend components

---
+++ backend-tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace backend_tests {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
