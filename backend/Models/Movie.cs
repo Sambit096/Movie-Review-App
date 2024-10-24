1992472ccf7f5c6f39bd6ae7e937129a6674eab7
Adams <keeferadams@gmail.com>
2024-10-24 16:56:12 -0400

    fixing file structure

--- backend/Models/Movie.cs
+++ backend/Models/Movie.cs
@@ -1,7 +1,8 @@
-namespace MovieReviewApp.Models
+namespace MovieReviewApp.backend.Models
 {
     public class Movie
     {
+        public required string MovieId { get; set; }
         public required string Title { get; set; }
         public string? Genre { get; set; }
         public string? Description { get; set; }
