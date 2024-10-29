30662b7cd3b39d93d0fd900e98b317e562d56cd7
Mark <markbob666@gmail.com>
2024-10-29 16:15:48 -0400

    cart interface

---
+++ Movie-Review-App/backend/Services/Interfaces/ICart.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace MovieReviewApp.models
+{
+    public interface ICart
+    {
+        int CartId { get; set; }
+        List<Ticket> Tickets { get; set; }
+        double Total { get; set; }
+
+        bool AddTicketToCart(int cartId, int ticketId, int quantity);
+        bool RemoveTicketFromCart(int cartId, int ticketId);
+        Cart GetCart(int cartId);
+        bool ProcessPayment(int cartId, string cardNumber, string exp, string cardHolderName, string cvc);
+    }
+}
