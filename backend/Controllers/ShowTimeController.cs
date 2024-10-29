cf8880d153066fc6f7057b2b01b6d6207ad594ff
Georgia Towne <georgiatowne@gmail.com>
2024-10-28 22:10:49 -0400

    ShowTime Controller/Interface Updates

--- backend/Controllers/ShowTimeController.cs
+++ backend/Controllers/ShowTimeController.cs
@@ -13,16 +13,55 @@ public class ShowTimeController : ControllerBase {
         this.showTimeService = showTimeService;
     }
 
+    /// <summary>
+    /// Gets all ShowTimes
+    /// </summary>
+    /// <returns></returns>
     [HttpGet(nameof(GetAllShowTimes))]
-    public IActionResult GetAllShowTimes() {
-        return Ok(this.showTimeService.GetAllShowTimes());
+    public async Task<IActionResult> GetAllShowTimes() {
+        try {
+            var showTimes = await this.showTimeService.GetAllShowTimes();
+            if (showTimes == null || !showTimes.Any()) {
+                return NotFound();
+            }
+            return Ok(showTimes);
+        } catch (Exception error) {
+            return StatusCode(500, $"Error when receiving ShowTime Data: {error}");
+        }
     }
+    /// <summary>
+    /// Gets all ShowTimes for a specific Movie based on Movie ID
+    /// </summary>
+    /// <param name="movieId"></param>
+    /// <returns></returns>
     [HttpGet("GetShowTimes/{movieId}")]
-    public IActionResult GetShowTimes(int movieId) {
-        return Ok(this.showTimeService.GetShowTimes(movieId));
+    public async Task<IActionResult> GetShowTimes(int movieId) {
+        try {
+            var showTimes = await this.showTimeService.GetShowTimes(movieId);
+            if (showTimes == null || !showTimes.Any()) {
+                return NotFound();
+            }
+            return Ok(showTimes);
+        } catch (Exception error) {
+            return StatusCode(500, $"Error when receiving ShowTime Data for movieId: {error}");
+        }
+        
     }
+    /// <summary>
+    /// Gets all Tickets for a specific movie (adds all from ShowTimes)
+    /// </summary>
+    /// <param name="movieId"></param>
+    /// <returns></returns>
     [HttpGet("GetTickets/{movieId}")]
-    public IActionResult GetTickets(int movieId) {
-        return Ok(this.showTimeService.GetTickets(movieId));
+    public async Task<IActionResult> GetTickets(int movieId) {
+        try {
+            var tickets = await this.showTimeService.GetTickets(movieId);
+            if (tickets == null) {
+                return NotFound("No Tickets with this movieId were found.");
+            } 
+            return Ok(tickets);
+        } catch (Exception error) {
+            return StatusCode(500, $"Error when receiving Ticket Data: {error}");
+        }
     }
 }
--- backend/Services/Implementations/ShowTimeService.cs
+++ backend/Services/Implementations/ShowTimeService.cs
@@ -2,6 +2,7 @@ using MovieReviewApp.Interfaces;
 using MovieReviewApp.Models;
 using System.Linq;
 using MovieReviewApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MovieReviewApp.Services {
     public class ShowTimeService : IShowTimeService {
@@ -10,21 +11,50 @@ namespace MovieReviewApp.Services {
         public ShowTimeService(MovieReviewDbContext dbContext) {
             this.dbContext = dbContext;
         }
-        public IList<ShowTime> GetAllShowTimes() {
-            List<ShowTime> allShowTimes = new List<ShowTime>();
-            allShowTimes = dbContext.ShowTimes.ToList();
-            return allShowTimes;
-        }
-        public IList<ShowTime> GetShowTimes(int movieId) {
-            var allShowTimes = dbContext.ShowTimes.Where(st => st.MovieId == movieId).ToList();
-            return allShowTimes;
-        }
 
-        public int GetTickets(int movieId) {   
-            var allShowTimes = (from st in dbContext.ShowTimes
-            where st.MovieId == movieId
-            select st.NumOfTickets).Sum();
-            return allShowTimes;
+        /// <summary>
+        /// Gets all ShowTimes from Database
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<IList<ShowTime>> GetAllShowTimes() {
+            try {
+                List<ShowTime> allShowTimes = new List<ShowTime>();
+                allShowTimes = await dbContext.ShowTimes.ToListAsync();
+                return allShowTimes;
+            } catch (Exception error) {
+                throw new Exception("Error when retreiving ShowTimes from Database:", error);
+            }
+        }
+        /// <summary>
+        /// Gets all ShowTimes for a specific movie based on MovieId from database
+        /// </summary>
+        /// <param name="movieId"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<IList<ShowTime>> GetShowTimes(int movieId) {
+            try {
+                var allShowTimes = await dbContext.ShowTimes.Where(st => st.MovieId == movieId).ToListAsync();
+                return allShowTimes;   
+            } catch (Exception error) {
+                throw new Exception("Error when retreiving ShowTimes from Database:", error);
+            }
+        }
+        /// <summary>
+        /// Gets all Tickets for a specific movie based on ShowTimes for the movie (by Movie Id)
+        /// </summary>
+        /// <param name="movieId"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<int> GetTickets(int movieId) {   
+            try {
+                var allShowTimes = await (from st in dbContext.ShowTimes
+                where st.MovieId == movieId
+                select st.NumOfTickets).SumAsync();
+                return allShowTimes;
+            } catch (Exception error) {
+                throw new Exception("Error when retreiving ShowTime Tickets from Database:", error);
+            }
         }
     
     }
