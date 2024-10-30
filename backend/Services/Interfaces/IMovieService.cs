0c91ce31352ae3845dd46486cb063b1d50c265fd
andyjDavison <andyd.soccer@gmail.com>
2024-10-30 15:32:01 -0400

    Added the GetMovieById GET method to Movie

--- backend/Services/Interfaces/IMovieService.cs
+++ backend/Services/Interfaces/IMovieService.cs
@@ -2,6 +2,7 @@ using MovieReviewApp.Models;
 namespace MovieReviewApp.Interfaces {
     public interface IMovieService {
         IList<Movie> GetMovies();
+        Movie GetMovieById(int id);
         bool AddMovie(Movie movie);
         bool RemoveMovie(Movie movie);
         bool EditMovie(Movie movie);
