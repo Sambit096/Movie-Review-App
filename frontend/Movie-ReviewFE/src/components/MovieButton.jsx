06041edf6a257091b9ba9c4711b855e35da1938a
Nicholas Reiss <nreiss@email.sc.edu>
2024-10-30 14:07:31 -0400

    removed test data

--- frontend/Movie-ReviewFE/src/components/MovieButton.jsx
+++ frontend/Movie-ReviewFE/src/components/MovieButton.jsx
@@ -5,7 +5,7 @@ const MovieButton = ({to, title}) => {
     const navigate = useNavigate();
 
     return(
-        <button onClick={() => {navigate(`/Movies/${to}`)}}>
+        <button className="buytickets--button" onClick={() => {navigate(`/Movies/${to}`)}}>
             <p>Buy Tickets</p>
         </button>
     )
