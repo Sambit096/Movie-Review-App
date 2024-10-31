8186eb200dc2d6620be3a78aaffdcaad97609bb5
adamskeefer <keeferadams@gmail.com>
2024-10-31 19:00:54 -0400

    ticket migration updated

---
+++ backend/Migrations/20241031230015_UpdateTicketSchema.cs
@@ -0,0 +1,452 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieReviewApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTicketSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 1,
+                column: "CartId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 2,
+                column: "CartId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 3,
+                column: "CartId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 4,
+                column: "CartId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 5,
+                column: "CartId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 6,
+                column: "CartId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 7,
+                column: "CartId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 8,
+                column: "CartId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 9,
+                column: "CartId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 10,
+                column: "CartId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 11,
+                column: "CartId",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 12,
+                column: "CartId",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 13,
+                column: "CartId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 14,
+                column: "CartId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 15,
+                column: "CartId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 16,
+                column: "CartId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 17,
+                column: "CartId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 18,
+                column: "CartId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 19,
+                column: "CartId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 20,
+                column: "CartId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 21,
+                column: "CartId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 22,
+                column: "CartId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 23,
+                column: "CartId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 24,
+                column: "CartId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 25,
+                column: "CartId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 26,
+                column: "CartId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 27,
+                column: "CartId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 28,
+                column: "CartId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 29,
+                column: "CartId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 30,
+                column: "CartId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 31,
+                column: "CartId",
+                value: 5);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 1,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 2,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 3,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 4,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 5,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 6,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 7,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 8,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 9,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 10,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 11,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 12,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 13,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 14,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 15,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 16,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 17,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 18,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 19,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 20,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 21,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 22,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 23,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 24,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 25,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 26,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 27,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 28,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 29,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 30,
+                column: "CartId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 31,
+                column: "CartId",
+                value: 0);
+        }
+    }
+}
