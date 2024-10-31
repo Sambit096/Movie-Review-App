f4fae564ac2c8e7a8285ee8fbfb7b91f92cde924
andyjDavison <andyd.soccer@gmail.com>
2024-10-31 17:26:46 -0400

    Add the user Model

---
+++ backend/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieReviewApp.Models {
+    public class User {
+        public int UserId { get; set; }
+        public string email {  get; set; }
+        public string username {  get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+    }
+}
